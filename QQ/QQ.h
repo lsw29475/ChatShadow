db63887470c34057163fbf3d26c9e627638ac526
lsw29475 <lsw29475@qq.com>
2024-08-02 15:13:38 +0800

    Add files via upload

--- QQ/QQ.h
+++ QQ/QQ.h
@@ -8,6 +8,8 @@
 #define QQ_PASSWORD_SIZE 16
 // 校验密钥长度
 #define QQ_CHECK_PASSWORD_SIZE 272
+// 打印输出间隔
+#define QQ_PROGRESS_INTERVAL 10000
 
 // 爆破QQ数据库文件密码并解密
 BOOL CrackQQMsgDBFile(const CHAR *szMemoryFilePath, const CHAR *szQQMsgDBFilePath, const CHAR *szDecQQMsgDBFilePath, const CHAR *szPasswordFilePath, int ThreadNum, BOOL blResume);
