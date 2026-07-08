1a4adfc3447a749eaab74a96c2423bf9cfd68a12
lsw29475 <lsw29475@users.noreply.github.com>
2026-07-08 14:38:30 +0800

    移除 wechat_v4 模块 (x'<hex>'格式未命中)

--- src/modules/wechat_v4.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../module.h"
-
-// WeChat 4.x module: SQLCipher 4 with raw key (WCDB format)
-// Key: 48-byte hex ASCII string = 32B enc_key + 16B salt
-// Format in memory: x'<64hex_enc><32hex_salt>'
-
-extern const ChatModule wechat_v4_module;
